bf2cde643ec57e8e2d54d17abecd7021e32e625c
jenyanewivanov <jenyanewivanov@gmail.com>
2026-04-24 15:15:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from flask import Flask
+import os
+import telebot
 
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(
+        message.chat.id,
+        "Привет! Я Оля Ассистент 🚀\nГотов помогать с продажами."
+    )
 
 @app.route("/")
 def home():
-    return "Olya is working 🚀"
+    return "Bot is running 🚀"
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
